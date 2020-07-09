0566f906972382dbb084806b8030a398ea6b7584
joycejosephtwenty20 <Joyce K Joseph@homenet.dmz>
2020-07-09 08:12:10 -0700

    4th commit

---
+++ .idea/js/nav.js
@@ -0,0 +1,23 @@
+document.getElementById("hover").addEventListener("mouseover", mouseOver);
+document.getElementById("hover").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("hover").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("hover").style.color = "white";
+}
+
+document.getElementById("hover").addEventListener("mouseover1", mouseOver);
+document.getElementById("hover").addEventListener("mouseout1", mouseOut);
+
+
+function mouseOver() {
+    document.getElementById("hover1").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("hover1").style.color = "white";
+}
+
